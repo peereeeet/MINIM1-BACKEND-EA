e13bee6ba3b64c45494f9c034a683b14f7fea822
peereeeet <pere.garces@estudiantat.upc.edu>
2024-11-11 20:27:35 +0100

    REPO INICI MINIM 1 (2)

--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -19,15 +19,13 @@ import {
     modificarNombreUsuarioPorId,
     modificarPasswordUsuarioPorId,
     modificarRolUsuarioPorId,
-    obtenerAsignaturasPaginadasDeUsuario,
-    loginUsuario
+    obtenerAsignaturasPaginadasDeUsuario
 } from '../controller/usuarioController';
 
 const router = express.Router();
 
 ////////////////////////////////////RUTAS SIN PARÁMETROS/////////////////////////////////////
 router.post('/', crearUsuario);  // Crear usuario
-router.post('/login', loginUsuario);
 router.get('/listar-paginados', obtenerUsuariosPaginados);  // Nueva ruta de paginación (asegúrate de que esté antes de las rutas con parámetros)
 router.get('/', listarUsuarios);  // Listar todos los usuarios
 
